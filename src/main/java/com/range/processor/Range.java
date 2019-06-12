686feae0d0f71b8a145edeae76a97a652c55d560
ashitasinghvi <51535381+ashitasinghvi@users.noreply.github.com>
2019-06-12 11:36:10 +0530

    Create Range.java

---
+++ src/main/java/com/range/processor/Range.java
@@ -0,0 +1,23 @@
+package com.range.processor;
+
+public class Range {
+	private int lowerRange;
+	private int upperRange;
+
+	public int getLowerRange() {
+		return lowerRange;
+	}
+
+	public void setLowerRange(int lowerRange) {
+		this.lowerRange = lowerRange;
+	}
+
+	public int getUpperRange() {
+		return upperRange;
+	}
+
+	public void setUpperRange(int upperRange) {
+		this.upperRange = upperRange;
+	}
+
+}
